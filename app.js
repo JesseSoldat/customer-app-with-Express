c4ff136bf71c9dfcc4373b9b7e8b2e0ac5752f06
jesse soldat <jsoldat@hotmail.com>
2016-08-14 09:00:29 +0700

    added a validator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 	bodyParser = require('body-parser'),
-	path = require('path');
+	path = require('path'),
+	expressValidator = require('express-validator');
 
 var app = express();
 
@@ -13,7 +14,30 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(expressValidator([]));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+	res.locals.errors = null;
+	next();
+});
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+})); 
 
 var people = [
 	{
@@ -32,12 +56,43 @@ var people = [
 		age: 70
 	}
 ];
+var title = 'Customers';
 
 app.get('/',  function(req, res){
 	// res.send('Hello World');
 	// res.json(people);
-	var title = 'Customers';
-	res.render('index', {title: title, people: people});
+	res.render('index', {title: title, people: people, errors: res.locals.errors});
+});
+
+app.post('/users/add', function(req, res){
+
+	req.checkBody('fName', 'First Name is Required!').notEmpty();
+	req.checkBody('lName', 'Last Name is Required!').notEmpty();
+	req.checkBody('email', 'Email is Required!').notEmpty();
+
+	errors = req.validationErrors();
+
+	if(errors){
+		res.render('index', {
+			title: title, 
+			people: people,
+			errors: errors
+		});
+	} else {
+
+		var newUser = {
+			fName: req.body.fName,
+			lName: req.body.lName,
+			email: req.body.email
+		};
+		console.log(newUser);
+		res.render('index', {
+			title: title, 
+			people: people,
+			errors: res.locals.errors
+		});	
+	}
+	
 });
 
 var port = 3000;
