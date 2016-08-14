e9bb2603409e3cb13b256e14a442c95b6285b329
jesse soldat <jsoldat@hotmail.com>
2016-08-14 07:40:54 +0700

    parsing json

--- app.js
+++ app.js
@@ -4,8 +4,32 @@ var express = require('express'),
 
 var app = express();
 
-app.get('/', function(req, res){
-	res.send('Hello World');
+// var logger = function(req, res, next){
+// 	console.log('Logging....');
+// 	next();
+// }
+// app.use(logger);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+var people = [
+	{
+		fName: 'Jesse',
+		lName: 'Soldat',
+		age: 35
+	},
+	{
+		fName: 'Nate',
+		lName: 'Soldat',
+		age: 37
+	}
+];
+
+app.get('/',  function(req, res){
+	// res.send('Hello World');
+	res.json(people);
 });
 
 var port = 3000;
